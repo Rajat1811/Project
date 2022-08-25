e401c96fe72651d51729dd9d94ee4935121d8e64
Rajat1811 <rajatbains73@gmail.com>
2022-08-26 01:24:04 +0530

    final

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "contact_book",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+#Pagination
+
+REST_FRAMEWORK={
+    'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':2,
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
+}
+
