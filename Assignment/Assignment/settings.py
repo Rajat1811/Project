2b2e5f4fb1bbe96b2e381a2a541245aa477e1b6e
Rajat1811 <rajatbains73@gmail.com>
2022-08-26 03:22:03 +0530

    3

--- Assignment/Assignment/settings.py
+++ Assignment/Assignment/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = "django-insecure-4w#$j+#3!@kimw)=ebnmr=n=gz^)t-2xxocndav!gl!x!j(6!9"
-SECRET_KEY = os.environ.get('SECRET_KEY' "django-insecure django-insecure-4w#$j+#3!@kimw)=ebnmr=n=gz^)t-2xxocndav!gl!x!j(6!9")
+SECRET_KEY = "django-insecure django-insecure-4w#$j+#3!@kimw)=ebnmr=n=gz^)t-2xxocndav!gl!x!j(6!9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
